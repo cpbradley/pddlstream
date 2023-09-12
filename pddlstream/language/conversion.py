9f0a34049895875551ae779791bfd188f7fd4a35
Aaron Ray <aaronray@mit.edu>
2023-09-12 10:05:00 -0400

    Update import locations for generic containers for python 3.10

--- pddlstream/language/conversion.py
+++ pddlstream/language/conversion.py
@@ -14,7 +14,7 @@ def replace_expression(parent, fn):
     if prefix == EQ:
         assert(len(parent) == 3)
         value = parent[2]
-        if isinstance(parent[2], collections.Sequence):
+        if isinstance(parent[2], collections.abc.Sequence):
             value = replace_expression(value, fn)
         return prefix, replace_expression(parent[1], fn), value
     elif prefix in (CONNECTIVES + OBJECTIVES):
